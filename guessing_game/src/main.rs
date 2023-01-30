bdb47cbd8e81d11d6be36366375df32d6557b265
MCLyonzo <joshualyons95@hotmail.com>
2023-01-30 15:49:35 +1100

    Progress 30/1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,39 @@
+use std::io; //Standard input-output library
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop{
+        println!("\nPlease input your guess... ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Not high enough!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("CORRECT! Exiting now..");
+                break
+            }
+        }
+    }
+
+    
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\joshu\Documents\Rust\ Projects\guessing_game\target\debug\guessing_game.exe: C:\Users\joshu\Documents\Rust\ Projects\guessing_game\src\main.rs
